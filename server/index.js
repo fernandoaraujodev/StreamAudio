644cbbd59da2143debcee2531b402b58722fbd0c
Fernando Arauj <fernandovcaraujo@gmail.com>
2022-03-16 23:30:08 -0300

    Configurando a camada service

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import server from './server.js'
+import config from './config.js'
 import { logger } from './util.js'
 
-server.listen(5000)
-.on('listening', () => logger.info('server rodando!'))
+server.listen(3000)
+.on('listening', () => logger.info(`server rodando em ${config.port}`))
---
+++ server/service.js
@@ -0,0 +1,40 @@
+import fsPromises from 'fs/promises'
+import fs from 'fs'
+
+import config from './config.js'
+import { join, extname } from 'path'
+const {
+    dir: {
+        publicDirectory
+    } 
+} = config
+
+export class Service {
+    createFileStream(filename) {
+        return fs.createReadStream(filename)
+    }
+
+    async getFileInfo(file) {
+        // file = home/index.js
+        const fullFilePath = join(publicDirectory, file)
+        
+        // calida se existe, se não estoura erro
+        await fsPromises.access(fullFilePath)
+        const fileType = extname(fullFilePath)
+        return {
+            type: fileType,
+            name: fullFilePath
+        }
+    }
+
+    async getFileStream(file) {
+        const {
+            name,
+            type
+        } = await this.getFileInfo(file)
+        return {
+            stream: this.createFileStream(name),
+            type 
+        }
+    }
+}
