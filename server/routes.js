957b3650c38b1fe6443b54e74f6f2d69b28bafc6
Fernando Arauj <fernandovcaraujo@gmail.com>
2022-03-18 19:27:16 -0300

    Configurando as rotas

--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,35 @@
-export function handler(request, response){
+import config from "./config.js"
+import { Controller } from "./controller.js"
+import { logger } from "./logger.js"
+
+const {
+    location,
+    pages: {
+        homeHTML
+    }
+} = config
+
+const controller = new Controller()
+async function routes(request, response){
+    const { method, url } =  request
+
+    if(method === 'GET' && url === '/'){
+        response.writeHead(302, {
+            'Location': location.home
+        })
+        response.end()
+    }
+
+    if(method === 'GET' && url === '/home'){
+        const {
+
+        } = await controller.getFileStream(homeHTML)
+    }
+
     return response.end('hello world')
+}
+
+export function handler(request, response){
+    return routes(request, response)
+    .catch(error => logger.error(`Deu xablau: ${error.stack}`))
 }
